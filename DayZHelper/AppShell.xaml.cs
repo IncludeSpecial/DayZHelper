61f07e1e0e31fda017c006458f2d181c53f5ec40
Rustam <refractory40@gmail.com>
2023-09-23 16:13:26 +0900

    First Create

---
+++ DayZHelper/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DayZHelper;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ DayZHelper/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DayZHelper;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
