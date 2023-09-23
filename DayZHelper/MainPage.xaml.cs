7698382e9a9b418fef852e429fe469535555ed4a
Rustam <refractory40@gmail.com>
2023-09-23 16:13:26 +0900

    Test App

---
+++ DayZHelper/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayZHelper
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberOfPasswordsEntry.Text, out int numberOfPasswords) && numberOfPasswords > 0)
+            {
+                var passwords = GeneratePasswords(numberOfPasswords);
+                PasswordsLabel.Text = string.Join(Environment.NewLine, passwords);
+            }
+            else
+            {
+                PasswordsLabel.Text = "Введите допустимое количество паролей:";
+            }
+        }
+
+        private List<string> GeneratePasswords(int numberOfPasswords)
+        {
+            var passwords = new List<string>();
+            var random = new Random();
+
+            while (passwords.Count < numberOfPasswords)
+            {
+                string newPassword = Guid.NewGuid().ToString("N").Substring(0, 4);
+
+                if (!passwords.Contains(newPassword))
+                {
+                    passwords.Add($"Пароль #{passwords.Count + 1}: {newPassword}");
+                }
+            }
+
+            return passwords;
+        }
+    }
+}
