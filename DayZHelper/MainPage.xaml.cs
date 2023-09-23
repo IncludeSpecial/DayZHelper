7bdcb1d3f5edd2f0b11938dfeca30771b9ca9f93
Rustam <refractory40@gmail.com>
2023-09-23 20:58:15 +0900

    Изменил Страницу о нас и чет сделал в raidcal

--- DayZHelper/MainPage.xaml.cs
+++ DayZHelper/MainPage.xaml.cs
@@ -3,57 +3,56 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 
-namespace DayZHelper
+namespace DayZHelper;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    private ObservableCollection<string> passwordsCollection;
+
+    public MainPage()
     {
-        private ObservableCollection<string> passwordsCollection;
+        InitializeComponent();
+        passwordsCollection = new ObservableCollection<string>();
+        PasswordsCollectionView.ItemsSource = passwordsCollection;
+    }
 
-        public MainPage()
+    private void GenerateButton_Clicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(NumberOfPasswordsEntry.Text, out var numberOfPasswords) && numberOfPasswords > 0)
         {
-            InitializeComponent();
-            passwordsCollection = new ObservableCollection<string>();
-            PasswordsCollectionView.ItemsSource = passwordsCollection;
+            var passwords = GeneratePasswords(numberOfPasswords);
+            PasswordsCollectionView.ItemsSource = passwords;
         }
-
-        private void GenerateButton_Clicked(object sender, EventArgs e)
+        else
         {
-            if (int.TryParse(NumberOfPasswordsEntry.Text, out int numberOfPasswords) && numberOfPasswords > 0)
-            {
-                var passwords = GeneratePasswords(numberOfPasswords);
-                PasswordsCollectionView.ItemsSource = passwords;
-            }
-            else
-            {
-                PasswordsCollectionView.ItemsSource = new List<string> { "Введите допустимое количество паролей:" };
-            }
+            PasswordsCollectionView.ItemsSource = new List<string> { "Введите допустимое количество паролей:" };
         }
+    }
 
 
-        private List<string> GeneratePasswords(int numberOfPasswords)
+    private List<string> GeneratePasswords(int numberOfPasswords)
+    {
+        var passwords = new List<string>();
+        var random = new Random();
+
+        var leftColumnCount = numberOfPasswords / 2;
+        var rightColumnCount = numberOfPasswords - leftColumnCount;
+
+        for (var i = 0; i < Math.Max(leftColumnCount, rightColumnCount); i++)
         {
-            var passwords = new List<string>();
-            var random = new Random();
-
-            int leftColumnCount = numberOfPasswords / 2;
-            int rightColumnCount = numberOfPasswords - leftColumnCount;
-
-            for (int i = 0; i < Math.Max(leftColumnCount, rightColumnCount); i++)
+            if (i < leftColumnCount)
             {
-                if (i < leftColumnCount)
-                {
-                    string leftPassword = random.Next(10000).ToString("D4");
-                    passwords.Add($"Пароль #{i + 1}: {leftPassword}");
-                }
-
-                if (i < rightColumnCount)
-                {
-                    string rightPassword = random.Next(10000).ToString("D4");
-                    passwords.Add($"Пароль #{leftColumnCount + i + 1} : {rightPassword}");
-                }
+                var leftPassword = random.Next(10000).ToString("D4");
+                passwords.Add($"Пароль #{i + 1}: {leftPassword}");
             }
 
-            return passwords;
+            if (i < rightColumnCount)
+            {
+                var rightPassword = random.Next(10000).ToString("D4");
+                passwords.Add($"Пароль #{leftColumnCount + i + 1} : {rightPassword}");
+            }
         }
+
+        return passwords;
     }
 }
